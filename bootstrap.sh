041e11be287cbdd7ecc4dedaeca2308e14e299bc
Takumi KINJO <takumi.kinjo@gmail.com>
2015-02-04 11:54:40 +0900

    Add git as default installed package.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Install Ansible
-sudo apt-get install -y python-dev python-pip sshpass
+sudo apt-get install -y python-dev python-pip sshpass git
 sudo easy_install pip
 sudo pip install ansible PyYaML Jinja2
