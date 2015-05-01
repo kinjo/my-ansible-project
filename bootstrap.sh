800a0f2c4bfe07f51e57274b9dc0d4f4b5d2e9bc
Takumi KINJO <takumi.kinjo@gmail.com>
2015-05-01 14:10:57 +0900

    Enable swap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Enable swap
+if [ "$(swapon -s | wc -l)" = "0" ]; then
+  sudo dd if=/dev/zero of=/swapfile bs=1024 count=512k
+  sudo mkswap /swapfile
+  sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+  sudo swapon -a
+fi
+
 # Install Ansible
 sudo apt-get update
 sudo apt-get dist-upgrade -y
